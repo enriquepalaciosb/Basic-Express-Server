948300455acc2c381a7a5a887f1ee37345e28373
Enrique Palacios Baron <71736745+enriquepalaciosb@users.noreply.github.com>
2021-11-17 12:40:43 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+app.get("/", function(req, res){
+  res.send("<h1>Hello, world!</h1>");
+});
+app.get("/contact", function(req, res){
+  res.send("Contact me @: enriquegpalacios@gmail.com");
+});
+app.get("/about", function(req, res){
+  res.send("Hello, my name is Enrique and I love Web Development");
+});
+app.get("/hobbies", function(req, res){
+  res.send("<ul> <li>Soccer</li> <li>Reading</li> <li>Gaming</li> </ul>");
+});
+app.listen(3000, function(){
+  console.log("server started on port 3000");
+});
