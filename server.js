0a94ffdf951fe468311c3892e3ddc4e36105d1ad
Enrique Palacios Baron <71736745+enriquepalaciosb@users.noreply.github.com>
2021-11-17 12:46:59 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.get("/contact", function(req, res){
   res.send("Contact me @: enriquegpalacios@gmail.com");
 });
 app.get("/about", function(req, res){
-  res.send("Hello, my name is Enrique and I love Web Development");
+  res.send("Hello, my name is Enrique and I love Web Development!");
 });
 app.get("/hobbies", function(req, res){
-  res.send("<ul> <li>Soccer</li> <li>Reading</li> <li>Gaming</li> </ul>");
+  res.send("<ul> <li>Soccer</li> <li>Watching Documentaries</li> <li>Learning</li> </ul>");
 });
 app.listen(3000, function(){
   console.log("server started on port 3000");
